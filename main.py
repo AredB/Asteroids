91df286ac86c7a3e88c0d8526a2303b3928a0013
AredB <aredbruutal@gmail.com>
2025-05-28 23:48:40 +0200

    Draw player

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # throughout this file
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     pygame.init()
@@ -12,11 +14,15 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     clock = pygame.time.Clock()
     dt = 0
+    x = SCREEN_WIDTH / 2
+    y = y=SCREEN_HEIGHT / 2
+    player = Player(x,y)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
