733d8575e9d04ab2e6fe9049e8c32153a25a20b1
AredB <aredbruutal@gmail.com>
2025-05-29 00:20:19 +0200

    Use containers

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from player import *
 
 def main():
     pygame.init()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -16,16 +18,20 @@ def main():
     dt = 0
     x = SCREEN_WIDTH / 2
     y = y=SCREEN_HEIGHT / 2
+    Player.containers = (updatable, drawable)
     player = Player(x,y)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-        player.update(dt)
+        updatable.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
         
         dt = clock.tick(60)/1000
